cfe898613b245379cf0f4475016299c6b7410378
James Bostock <James.Bostock@checkmarx.com>
2021-07-28 11:16:00 +1000

    Add license

--- gitlab-webhook-flask.py
+++ gitlab-webhook-flask.py
@@ -1,9 +1,22 @@
 # gitlab-webhook-flask.py
 #
-# Copyright 2021 Checkmarx
-#
 # A hanfler for GitLab Webhooks. Currently only handles push events
 # sent upon branch deletion.
+#
+# Copyright 2021 Checkmarx
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from http import HTTPStatus
 from flask import Flask, request, jsonify
 from logging.config import dictConfig
