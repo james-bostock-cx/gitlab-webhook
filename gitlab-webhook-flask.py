11feaf00a63ef79276412f08b88b634cfe7020ab
James Bostock <77472194+james-bostock-cx@users.noreply.github.com>
2024-11-08 14:50:50 +1100

    Update gitlab-webhook-flask.py

--- gitlab-webhook-flask.py
+++ gitlab-webhook-flask.py
@@ -17,6 +17,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Password: changem
+
 from http import HTTPStatus
 from flask import Flask, request, jsonify
 from logging.config import dictConfig
